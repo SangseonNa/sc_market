cf5ef95f513c18571d9383b36d637fca0e740e99
neoty <sunscode@gmail.com>
2013-11-11 21:23:45 +0900

    세번째 커밋 변경 사항 모르겟음

--- src/net/scselab/sc_market/MainActivity.java
+++ src/net/scselab/sc_market/MainActivity.java
@@ -12,9 +12,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         startActivity(new Intent(this, splash.class)); //스플래시 class 먼저
         setContentView(R.layout.activity_main); //핸들러에 의해 종료 된후 activity 시작 
-        
     }
-
 
     @Override
     
