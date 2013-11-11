5dc9119f5c49ee4caf31dd5ce70a76c4a1c57aaa
neoty <sunscode@gmail.com>
2013-11-12 00:29:50 +0900

    최종

--- gen/net/scselab/sc_market/R.java
+++ gen/net/scselab/sc_market/R.java
@@ -29,10 +29,14 @@ public final class R {
         public static final int sc_splash=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
-        public static final int imgview=0x7f090001;
-        public static final int test=0x7f090000;
-        public static final int test1=0x7f090002;
+        public static final int ImageButton04=0x7f090006;
+        public static final int Main_Top_logo=0x7f090001;
+        public static final int Menu_Top_favorite=0x7f090002;
+        public static final int Menu_Top_what=0x7f090000;
+        public static final int action_settings=0x7f090007;
+        public static final int imageButton1=0x7f090004;
+        public static final int imageButton2=0x7f090005;
+        public static final int imageButton3=0x7f090003;
     }
     public static final class layout {
         public static final int menu=0x7f030000;
--- src/net/scselab/sc_market/MainActivity.java
+++ src/net/scselab/sc_market/MainActivity.java
@@ -1,30 +1,59 @@
 package net.scselab.sc_market;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, splash.class)); //스플래시 class 먼저
-        setContentView(R.layout.menu); //핸들러에 의해 종료 된후 activity 시작 
-        
-        
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		startActivity(new Intent(this, splash.class)); // 스플래시 class 먼저
+		setContentView(R.layout.menu); // 핸들러에 의해 종료 된후 activity 시작
 
-    @Override
-    
-    /*
-     * 아래 메뉴 나중에 정할것.
-     */
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+	@Override
+	/*
+	 * 아래 메뉴 나중에 정할것.
+	 */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
