29b217745d41dccbdbc962c2b1e901a475e0cc08
neoty <sunscode@gmail.com>
2013-11-12 02:40:01 +0900

    메뉴쪽에 아이콘 추가

--- gen/net/scselab/sc_market/R.java
+++ gen/net/scselab/sc_market/R.java
@@ -25,8 +25,10 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int sc_splash=0x7f020001;
+        public static final int bookmarks=0x7f020000;
+        public static final int db_settings=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int sc_splash=0x7f020003;
     }
     public static final class id {
         public static final int Main_Top_logo=0x7f090001;
