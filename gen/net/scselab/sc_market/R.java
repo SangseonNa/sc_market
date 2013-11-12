015c66015efe736b83df1e686d170f7c33b7ebbb
neoty <sunscode@gmail.com>
2013-11-12 15:55:10 +0900

    11월 12일 4시 수정본 추가 사항 menu.xml 다듬기 및 상당 버튼 이벤트 걸기 확인및
    롤오버 추가

--- gen/net/scselab/sc_market/R.java
+++ gen/net/scselab/sc_market/R.java
@@ -11,8 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int bg=0x7f050001;
-        public static final int top_bg=0x7f050000;
+        public static final int bg=0x7f040003;
+        public static final int cyan=0x7f040000;
+        public static final int top_bg=0x7f040001;
+        public static final int top_under_bg=0x7f040002;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -21,14 +23,16 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int bookmarks=0x7f020000;
         public static final int db_settings=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int sc_splash=0x7f020003;
+        public static final int header_bg=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int sc_splash=0x7f020004;
+        public static final int selector=0x7f020005;
     }
     public static final class id {
         public static final int Main_Top_logo=0x7f090001;
--- src/net/scselab/sc_market/MainActivity.java
+++ src/net/scselab/sc_market/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements View.OnClickListener {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -24,37 +23,63 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		MenuBtn2.setOnClickListener(this);
 		MenuBtn3.setOnClickListener(this);
 		MenuBtn4.setOnClickListener(this);
+		
+
+		final ImageButton MenuBtnwhat = (ImageButton) findViewById(R.id.Menu_Top_what);
+		MenuBtnwhat.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 테스트용 토스트
+				
+				Toast toast1 = Toast.makeText(getApplicationContext(), "hello",
+						Toast.LENGTH_SHORT);
+				toast1.show();
+			}
+		});
+		final ImageButton MenuBtnfavorite = (ImageButton) findViewById(R.id.Menu_Top_favorite);
+		MenuBtnfavorite.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 테스트용 토스트
+				Toast toast2 = Toast.makeText(getApplicationContext(), "hello",
+						Toast.LENGTH_SHORT);
+				toast2.show();
+			}
+		});
+
 	}
-
-
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.Menu_btn_1:
-			//1번 토스트 테스트
+			// 1번 토스트 테스트
+
 			Toast toast1 = Toast.makeText(this, "1", Toast.LENGTH_SHORT);
 			toast1.show();
 			break;
 		case R.id.Menu_btn_2:
-			//2번 토스트 테스트
+			// 2번 토스트 테스트
 			Toast toast2 = Toast.makeText(this, "2", Toast.LENGTH_SHORT);
 			toast2.show();
 			break;
 		case R.id.Menu_btn_3:
-			//3번 토스트 테스트
+			// 3번 토스트 테스트
 			Toast toast3 = Toast.makeText(this, "3", Toast.LENGTH_SHORT);
 			toast3.show();
 			break;
 		case R.id.Menu_btn_4:
-			//4번 토스트 테스트
+			// 4번 토스트 테스트
 			Toast toast4 = Toast.makeText(this, "4", Toast.LENGTH_SHORT);
 			toast4.show();
 			break;
 		}
 
 	}
+
 	@Override
 	/*
 	 * 아래 메뉴 나중에 정할것.
@@ -64,7 +89,5 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	
-	
+
 }
